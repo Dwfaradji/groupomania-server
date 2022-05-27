d6dac545e19c27c0c5a25f86b30129263992f582
Dwfaradji <85832124+Dwfaradji@users.noreply.github.com>
2022-05-27 10:54:37 +0200

    fichier a la racine

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ appli.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type:mulipart/from-data, Authorization"
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
